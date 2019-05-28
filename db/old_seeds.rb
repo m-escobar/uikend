1ba35183c0f81b0d262566d9bd51e4c84c7c122d
Marcelo Escobar <escobar@br.systems>
2019-05-28 16:40:08 -0300

    real seed info

---
+++ db/old_seeds.rb
@@ -0,0 +1,33 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Seeding DB"
+
+Trip.delete_all
+
+user = User.first
+
+30.times {
+    t = Trip.new
+
+    t.name = Faker::FunnyName.three_word_name 
+    t.description = Faker::Lorem.sentences(1, true)
+    t.place = Faker::Address.city
+    t.capacity =  Faker::Number.between(5, 38)
+    t.hotel = Faker::Company.name
+    t.schedule = Faker::Lorem.sentence(30, true, 4)
+    t.price = Faker::Number.between(249, 789)
+    t.end = Faker::Date.forward(90)
+    t.start = t.end - 2.days
+    t.user = user
+
+    t.save
+}
+
+puts "Now you have #{Trip.count} Trips"
