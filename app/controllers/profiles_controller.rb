d5baa28f3ca4a8b7c19db15c188725160c244667
RaphaelAmorim Schmidt <raphaschmidt89@gmail.com>
2019-05-30 16:49:22 -0300

    pastas do profile criadas

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,21 @@
+class ProfilesController < ApplicationController
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :trips
   has_many :bookings
+  has_one :profile
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
